0c7718e5bb7c56555083775dcd7981e4f0055c03
LipPooo <alifm0574@gmail.com>
2026-05-28 16:48:49 +0800

    Add Home button to About overlay (top-left, mirrors close button)

--- resources/views/partials/about-overlay.blade.php
+++ resources/views/partials/about-overlay.blade.php
@@ -22,6 +22,14 @@
         </defs>
     </svg>
 
+    {{-- Home button (top-left) --}}
+    <a href="/" class="about-home js-about-close" aria-label="Go to homepage">
+        <svg width="17" height="17" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.2" stroke-linecap="round" stroke-linejoin="round">
+            <path d="M3 9.5L12 3l9 6.5V20a1 1 0 01-1 1H5a1 1 0 01-1-1V9.5z"/>
+            <path d="M9 21V12h6v9"/>
+        </svg>
+    </a>
+
     {{-- Close button (top-right) --}}
     <button class="about-close js-about-close" aria-label="Close About navigation">
         <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round">
